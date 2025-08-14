292d9c4ad8c6079ef0706926bc46978b840a2bf0
Shreelekha Saha <sahashreelekha84@gmail.com>
2025-08-14 15:09:13 +0530

    updated

--- server/app/model/usermodel.js
+++ server/app/model/usermodel.js
@@ -1,5 +1,45 @@
 const mongoose=require('mongoose')
 const Schema=mongoose.Schema
+const Joi = require('joi');
+
+const userschemaValidate = Joi.object({
+  name: Joi.string()
+    .pattern(/^[A-Z][a-z]*\s[A-Z][a-z]*$/)
+    .required()
+    .messages({
+      'string.pattern.base': 'Name must be two words with each starting with a capital letter.',
+    }),
+
+  email: Joi.string()
+    .email()
+    .pattern(/@(gmail\.com|yahoo\.com|yopmail\.com)$/)
+    .required()
+    .messages({
+      'string.pattern.base': 'Email must be from gmail.com, yahoo.com, or yopmail.com.',
+    }),
+
+  password: Joi.string()
+    .pattern(/^[A-Za-z](?=.*[0-9])(?=.*[^A-Za-z0-9]).{5,}$/)
+    .required()
+    .messages({
+      'string.pattern.base': 'Password must start with a letter, contain at least one number and one special character.',
+    }),
+
+  profileImg: Joi.string().optional(),
+
+  role: Joi.string().valid('admin', 'Doctor', 'Patient').optional(),
+
+  address: Joi.object({
+    line1: Joi.string().allow(''),
+    line2: Joi.string().allow(''),
+  }).optional(),
+
+  gender: Joi.string().valid('Male', 'Female', 'Other', 'Not Selected').optional(),
+
+  dob: Joi.string().optional(),
+
+  phone: Joi.string().pattern(/^\d{10}$/).optional(),
+});
 const userSchema=new Schema({
 
   name: {type:String,required:true},
@@ -16,4 +56,4 @@ const userSchema=new Schema({
 
 },{timestamps:true})
 const userModel=mongoose.model('user',userSchema)
-module.exports=userModel
+module.exports={userModel,userschemaValidate}
