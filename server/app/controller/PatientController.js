5bb741eb507df67a35a560102468e0c421a6a36d
Shreelekha Saha <sahashreelekha84@gmail.com>
2025-08-14 15:57:29 +0530

    updated

--- server/app/controller/PatientController.js
+++ server/app/controller/PatientController.js
@@ -44,7 +44,7 @@ class PatientController {
                 subject: 'OTP Verification',
 
                 text: `Your OTP is: ${otp}`,
-                html: `<p>Dear ${data.fullname},</p><p>Thank you for signing up with our website. To complete your registration, please verify your email address by entering the following one-time password (OTP)</p>
+                html: `<p>Dear ${data.name},</p><p>Thank you for signing up with our website. To complete your registration, please verify your email address by entering the following one-time password (OTP)</p>
                        <h2>OTP: ${otp}</h2>
                        <p>This OTP is valid for 10 minutes. If you didn't request this OTP, please ignore this email.</p>`
             })
