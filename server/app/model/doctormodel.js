3d78fdc7d863a840245bf3a200cc684d09d1b150
Shreelekha Saha <sahashreelekha84@gmail.com>
2025-09-16 01:49:27 +0530

    updated

--- server/app/model/doctormodel.js
+++ server/app/model/doctormodel.js
@@ -6,17 +6,17 @@ const doctorSchema = new Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     profileImg: { type: String, default: "" },
-    role:{type:String,default:'Doctor'},
+    roleId: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
     specialization: { type: String, required: true },
-    degree:{ type: String, required: true },
-    experience:{ type: String, required: true },
-    about:{ type: String, required: true },
-    fees:{ type: Number, required: true },
+    degree: { type: String, required: true },
+    experience: { type: String, required: true },
+    about: { type: String, required: true },
+    fees: { type: Number, required: true },
     location: { type: String, required: true },
 
     phone: { type: String, default: '0000000000' },
 
-   
+
 
 }, { timestamps: true })
 const doctorModel = mongoose.model('doctor', doctorSchema)
