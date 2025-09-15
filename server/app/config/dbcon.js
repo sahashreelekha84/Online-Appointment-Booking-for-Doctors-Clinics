ce7dc0491d180900ed989d4b9e043cc28008fee6
Shreelekha Saha <sahashreelekha84@gmail.com>
2025-09-16 01:40:18 +0530

    updated

--- server/app/config/dbcon.js
+++ server/app/config/dbcon.js
@@ -1,14 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const createRoles = require('../seeds/roleSeeder');
+// const seedAdmin = require('../seeds/seedAdmin');
 const dbcon = async () => {
     try {
         const db = await mongoose.connect(process.env.MONGODB_URL)
         if (db) {
             console.log('Database Connected Successfully');
+            try {
+                await createRoles();
+                console.log('All roles seeded successfully!');
+            } catch (e) {
+                console.log('Roles seeding failed:', e.message);
+            }
 
+            // try {
+            //     await seedAdmin();
+            //     console.log('Admin seeding checked!');
+            // } catch (e) {
+            //     console.log('Admin seeding failed:', e.message);
+            // }
         }
     } catch (error) {
         console.log('Database Connection fail');
 
     }
 }
-module.exports=dbcon
+module.exports = dbcon
